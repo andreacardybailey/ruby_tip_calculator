0fb5d1d72e22368b8799f7e51adf0093f80778e6
Andrea Cardy <acardy@customink.com>
2017-04-20 14:52:04 -0400

    Simple example of an online tip calculator using Sinatra.

---
+++ calculator.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'bigdecimal'
+require 'bigdecimal/util'
+
+set :views, "views"
+
+get '/' do
+    erb :index
+end
+
+post '/' do
+    bill = params[:bill].to_d
+    num_people = params[:num_people].to_d
+
+    tip = bill * 0.2
+    total_bill = bill + tip
+
+    ind_pay = total_bill/num_people
+
+    erb :results, :locals => {'total_bill' => sprintf('%.2f',total_bill), 'tip' => sprintf('%.2f',tip), 'ind_pay' => sprintf('%.2f', ind_pay)}
+    
+end
