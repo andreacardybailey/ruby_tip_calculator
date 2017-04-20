a8a89c4b77a3bbc2edc48cfd5ec3a47b4d30334b
Andrea Cardy <acardy@customink.com>
2017-04-20 14:59:19 -0400

    debugging heroku setup

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
-require 'bigdecimal'
-require 'bigdecimal/util'
+# require 'bigdecimal'
+# require 'bigdecimal/util'
 
 set :views, "views"
 
@@ -9,8 +9,8 @@ get '/' do
 end
 
 post '/' do
-    bill = params[:bill].to_d
-    num_people = params[:num_people].to_d
+    bill = params[:bill].to_f
+    num_people = params[:num_people].to_f
 
     tip = bill * 0.2
     total_bill = bill + tip
@@ -18,5 +18,5 @@ post '/' do
     ind_pay = total_bill/num_people
 
     erb :results, :locals => {'total_bill' => sprintf('%.2f',total_bill), 'tip' => sprintf('%.2f',tip), 'ind_pay' => sprintf('%.2f', ind_pay)}
-    
+
 end
